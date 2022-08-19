58aa84bdc5944f47d85db26cbe2d305f001c2d34
gary1999 <shengary1999@gmail.com>
2022-08-19 19:49:52 +1200

    Testing JSON

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
-let foods = `[
-    {
-        "restaurant":"McDonalds",
-        "rating": 1,
-        "review-notes": "HOT TRASH"
-    },
-    {
-        "restaurant":"Burger King",
-        "rating": 1000000,
-        "review-notes": "GOOD"
-    }
-]`
+// console.log(JSON.parse(foods)[0].restaurant);
 
-console.log(JSON.parse(foods)[0].restaurant);
+const fs = require('fs');
+
+fs.readFile('./Foods.json', 'utf-8', (err, jsonString) => {
+    if(err) {
+        console.log("File read failed:", err);
+        return;
+    }
+    else {
+        try {
+            const data = JSON.parse(jsonString);
+            console.log(data[0].restaurant);
+        }
+        catch (err) {
+            console.log('Error parsing JSON', err);
+        }
+
+    }
+})
