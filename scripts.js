6a9ca05d10f84e30176a0b329cc122766399c252
gary1999 <shengary1999@gmail.com>
2022-08-23 02:22:42 +1200

    Update on input

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 let reviews = [];
 
-const addReview = () => {
+const addReview = (event) => {
+
+    // event.preventDefault();
 
     // let restaurantID = document.getElementById("restaurant-name");
     let restaurantIDInputValue = document.getElementById("restaurant-name").value;
     let restaurantFoodInputValue = document.getElementById("food-name").value;
-
+    let restaurantReviewRatingValue;
+    let restaurantReviewInputValue = document.getElementById("review-notes").value;
 
     console.log(restaurantIDInputValue);
     console.log(restaurantFoodInputValue);
+    console.log(restaurantReviewInputValue);
     
 }
 
