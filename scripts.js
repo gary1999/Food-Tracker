0e7174c4f5cc63864144a1e8c774a033d793c1eb
gary1999 <shengary1999@gmail.com>
2022-08-23 15:21:36 +1200

    Update form

--- scripts.js
+++ scripts.js
@@ -2,19 +2,35 @@ let reviews = [];
 
 const addReview = (event) => {
 
-    // event.preventDefault();
+    let reviewList = [];
+
+    event.preventDefault();
 
     // let restaurantID = document.getElementById("restaurant-name");
-    let restaurantIDInputValue = document.getElementById("restaurant-name").value;
+    let restaurantNameInputValue = document.getElementById("restaurant-name").value;
     let restaurantFoodInputValue = document.getElementById("food-name").value;
     let restaurantReviewRatingValue;
     let restaurantReviewInputValue = document.getElementById("review-notes").value;
 
-    console.log(restaurantIDInputValue);
-    console.log(restaurantFoodInputValue);
-    console.log(restaurantReviewInputValue);
+    let reviewItem = {
+        name: restaurantNameInputValue,
+        // rating: 
+        food: restaurantFoodInputValue,
+        notes: restaurantReviewInputValue
+    }
+
+    reviewList.push(reviewItem);
+    document.forms[0].reset();
     
+    console.warn('added', {reviewList});
+    let pre = document.querySelector('#msg pre');
+    pre.textContent = '\n' + JSON.stringify(reviewList, '\t', 2);
+
 }
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById("submit-button").addEventListener('click', addReview);
+})
 
 console.log("Hello World");
 console.log("Test")
