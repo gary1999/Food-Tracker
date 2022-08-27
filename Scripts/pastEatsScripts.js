f6be189e6e959fbf715723da831071b4fe3163a6
gary1999 <shengary1999@gmail.com>
2022-08-28 07:55:13 +1200

    Card update
    Updated cards with Title + Ratings
    playing around with transitions and placement of items inside the card

--- Scripts/pastEatsScripts.js
+++ Scripts/pastEatsScripts.js
@@ -12,16 +12,34 @@ const initialise = () => {
 
     console.log(localStorageList);
 
+}
 
+const displayCards = () => {
+    
     for (let i = 0; i < localStorageList.length; i++){
-        console.log(localStorageList[i]);
+        console.log(localStorageList[i].rating);
         
+        //Create Card
         const restaurantCard = document.createElement('div');
         restaurantCard.classList = 'card';
+
+        //Create Card Title
+        const restaurantCardTitle = document.createElement('div');
+        restaurantCardTitle.classList = 'cardTitle';
+        restaurantCardTitle.innerHTML = (`${localStorageList[i].restaurant}`);
+        restaurantCard.appendChild(restaurantCardTitle);
+
+        //Create Card Ratings
+        const restaurantCardRating = document.createElement('div');
+        restaurantCardRating.classList = 'rating';
+        restaurantCardRating.innerHTML = (`Your Rating: ${localStorageList[i].rating}`);
+        restaurantCard.appendChild(restaurantCardRating);
+
+        //Append to container div in HTML
         document.getElementById("past-eats-container").appendChild(restaurantCard);
 
     }
-
 }
 
 initialise();
+displayCards();
